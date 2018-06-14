001cfa8af191554f2e4112e01a0370d091f974da
Christopher Beards <beardscm@gmail.com>
2018-06-14 13:07:18 -0400

    Volley

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 // Support for environmental variables
 require('dotenv/config');
-
 // Botkit package
 const Botkit = require('botkit');
-
 // Controller for the Slackbot
 const controller = Botkit.slackbot({
   debug: false
 });
-
 // Connect the bot to your Slack app using Token
 controller.spawn({
   token: process.env.TOKEN
 }).startRTM();
 
+//------------------------------------------------------
+//                  General Functionality
+//------------------------------------------------------
+
 // Determines behaviour
-controller.hears(['hello', 'hi'], ['ambient', 'direct_mention', 'mention'], function (bot, message) {
-  bot.reply(message, "Hello.");
+controller.hears(['hello', 'hi', 'what\'s up'], ['direct_mention', 'mention'], function (bot, message) {
+  bot.reply(message, "Hello");
 });
 
-controller.hears(['what does your name mean?'], ['direct_mention', 'mention'], function (bot, message) {
-  bot.reply(message, "My given name is Greek for _without a name_ or _namelessness_");
+
+//------------------------------------------------------
+//                    JavaScript
+//------------------------------------------------------
+
+controller.hears([/'what is JavaScript?'/], ['direct_mention', 'mention'], function (bot, message) {
+  bot.reply(message, "JavaScript, often abbreviated as JS, is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm.");
 });
